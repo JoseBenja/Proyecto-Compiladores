d72c613da1e4eb645338cb1d7bdddd1e151e7369
JoseBenja <jose.benjamin.mendez@hotmail.com>
2023-06-01 23:34:32 -0600

    Lectura de main, metodo y variables

---
+++ archivos/prueba.java
@@ -0,0 +1,10 @@
+public class ClaseMayor {
+
+    public static void main(String[] args) {
+        ClaseMayor cm = new ClaseMayor();
+    }
+
+    public String[] Prueba() {
+        return num;
+    }
+}
--- src/main/java/com/umg/proyectocompiladores/controller/CompiladorController.java
+++ src/main/java/com/umg/proyectocompiladores/controller/CompiladorController.java
@@ -10,15 +10,21 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
-import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import umg.compiladores.*;
 
 @RestController
 public class CompiladorController {
 
     StringBuilder builder = new StringBuilder();
 
-    String nomArchivoTexto = "C:\\Users\\VICTUS\\Archivo-Compilador\\codigoCompilar.java";
+    String nomArchivoTexto = "C:/Users/VICTUS/Archivo-Compilador/codigoCompilar.java";
 
     @PostMapping(value = "/recibirJava")
     public void recibirJava(@RequestParam("txtJava") String textoJava,
@@ -63,9 +69,21 @@ public class CompiladorController {
         try (BufferedWriter escritor = new BufferedWriter(new FileWriter(nomArchivoTexto))) {
             // Escribe la entrada de texto en el archivo
             escritor.write(txtJava);
-            System.out.println("El archivo se ha creado correctamente.");
+            compilarCup();
         } catch (IOException e) {
             System.out.println("Ocurrió un error al crear el archivo: " + e.getMessage());
+        }
+    }
+
+    public void compilarCup() {
+        try {
+            Reader reader = new FileReader("archivos/prueba.java");
+            parser p = new parser(new Lexer(reader));
+            Object result = p.parse().value;
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(CompiladorController.class.getName()).log(Level.SEVERE, "Error al generar en cup ", ex);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
