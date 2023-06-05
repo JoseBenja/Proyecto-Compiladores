f3330172a1124425f9ad929188a5d3e7a4930cc8
JoseBenja <jose.benjamin.mendez@hotmail.com>
2023-06-05 16:51:23 -0600

    Fase 02

--- codigoCompilar.java
+++ codigoCompilar.java
@@ -1,10 +1,10 @@
-public class ClaseMayor {
-
+public class aaczxcas ClaseMayor {
+int = 0;
     public static void main(String[] args) {
     }
 
     public String[] Prueba(String num, String num2) {
         int num0 = num2 + num;
-        return num2;
-    }
+        return num2;
+    }
 }
